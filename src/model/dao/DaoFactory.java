2a22ed10875796d35142bd5dc8d2c8fa72ea5714
Pedro Martins <pedromartinsdelemos@gmail.com>
2024-08-13 15:31:50 -0300

    Rewriting notes by DaoFactory

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -3,8 +3,7 @@ package model.dao;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
-	public static SellerDao createSellerDao() { // expõe um método que return o tipo da interface
+	public static SellerDao createSellerDao() { //Expõe somente um método que return o tipo da interface
 		return new SellerDaoJDBC();// instancia uma implementação
-		//Não expondo a implementação, somenta a interface
 	}
 }
