66c1f11c8ef358f8ed4110e551b3333a2ec64788
Pedro Martins <pedromartinsdelemos@gmail.com>
2024-08-13 14:12:56 -0300

    Creating project and git repository

---
+++ src/db/DbIntregityException.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DbIntregityException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbIntregityException(String msg) {
+		super(msg);
+	}
+
+}
